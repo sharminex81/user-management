b22908bca2fc611c72c08180198df0f46ae0f534
sharminex81 <mimetyshanta@gmail.com>
2017-12-08 02:40:54 +0600

    Initial Commit

---
+++ app/Http/Models/UsersModel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sharmin
+ * Date: 12/8/17
+ * Time: 1:12 AM
+ */
+
+//namespace App\Http\Models;
+namespace Besofty\Web\Accounts\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsersModel extends Model
+{
+    public function test()
+    {
+        var_dump(3333); die();
+    }
+}
