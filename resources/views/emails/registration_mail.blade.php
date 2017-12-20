a0a13b044147e51ff36c2d26704efcb2eb8f81f6
sharminex81 <mimetyshanta@gmail.com>
2017-12-21 03:08:23 +0600

    File name has renamed

---
+++ resources/views/emails/registration_mail.blade.php
@@ -0,0 +1,16 @@
+
+<p>
+    Hello! {{ $receiver_name }} from {{ $requestIP }} has requested an account on the {{ $siteUrl }}.
+</p>
+
+ <p>
+     Please go to {{ $link }} in order to complete this request.
+ </p>
+
+<p>
+    Once you click this link, your request will be reviewed, and you will shortly receive a separate email with more information.
+</p>
+
+<p>
+    If you did not make this request, please disregard this message.
+</p>
